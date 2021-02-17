37cb9163f90d9c835c52d18fa56a98b38979c818
Rananjaya <rananjaya@vizuamatix,com>
2021-02-17 14:25:27 +0530

    redux 1

---
+++ counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const Counter = () =>(
+ 
+    <View style={{flex :1,justifyContent : "center",alignItems:"center"}}>
+        <Text>123</Text>
+    </View>
+
+);
+
+export default Counter;
